1b9cba69f5621ea5e4785248709cda85bbb2c030
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-06 15:29:18 +0200

    Updated productdetails.php to show similar items

--- productdetails.php
+++ productdetails.php
@@ -1,8 +1,9 @@
 <?php
 
 include('connection.php');
-
+//Product details
 $id = $_GET["id"];
+$category=$_GET["category"];
 
 $query= $mysqli->prepare("select * from items where item_id=?");
 $query->bind_param("i",$id);
@@ -13,8 +14,26 @@ while ($object = $result->fetch_assoc()){
     $data = $object;
 }
 
-$response["product"] = $data;
+$response["product"] = $data; 
 
 echo json_encode($response);
+
+
+
+//Similar products
+
+$querySimilar=$mysqli->prepare("select * from items where item_category=?");
+$querySimilar->bind_param("i",$category);
+$querySimilar->execute();
+$resultSimilar = $querySimilar->get_result();
+
+while($objectSimilar = $resultSimilar->fetch_assoc()){
+    $array=$objectSimilar;
+}
+
+$responseSimilar['similar']=$array;
+
+echo json_encode($responseSimilar);
+
 
 ?>
